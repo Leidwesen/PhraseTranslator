9950c8b1ef45d424315bac344ced100a4f7133f5
TobiasDalton <tjd052799@gmail.com>
2023-09-06 19:06:43 -0400

    added texts for 0.281.3 APK

--- js/extras.js
+++ js/extras.js
@@ -1,55 +1,54 @@
 OVERLAPS = {
-    ['23•English']:'N'
-    , ['1243•English']:'M', 
-    ['3•Thai']:'S'
-    , ['269•Thai']:'N', 
-    ['23•Hindi']:'N'
-    , ['1243•Hindi']:'M', 
-    ['23•Brazilian Portuguese']:'N'
-    , ['1243•Brazilian Portuguese']:'M', 
-    ['23•Spanish']:'N'
-    , ['1243•Spanish']:'M', 
-    ['23•Turkish']:'N'
-    , ['1243•Turkish']:'M', 
-    ['79•Chinese Traditional']:'N'
-    , ['1241•Chinese Traditional']:'M', 
-    ['79•French']:'N'
-    , ['1241•French']:'M', 
-    ['79•Indonesian']:'N'
-    , ['1241•Indonesian']:'M', 
-    ['79•Turkish']:'N'
-    , ['1241•Turkish']:'M', 
-    ['16•French']:'N'
-    , ['1132•French']:'M', 
-    ['16•Indonesian']:'N'
-    , ['1132•Indonesian']:'M', 
-    ['17•Japanese']:'N'
-    , ['182•Japanese']:'N'
+['22•English']:'N'
+, ['1247•English']:'M', 
+['2•Thai']:'S'
+, ['269•Thai']:'N', 
+['22•Hindi']:'N'
+, ['1247•Hindi']:'M', 
+['22•Brazilian Portuguese']:'N'
+, ['1247•Brazilian Portuguese']:'M', 
+['22•Spanish']:'N'
+, ['1247•Spanish']:'M', 
+['22•Turkish']:'N'
+, ['1247•Turkish']:'M', 
+['79•Chinese Traditional']:'N'
+, ['1245•Chinese Traditional']:'M', 
+['79•French']:'N'
+, ['1245•French']:'M', 
+['79•Indonesian']:'N'
+, ['1245•Indonesian']:'M', 
+['79•Turkish']:'N'
+, ['1245•Turkish']:'M', 
+['15•French']:'N'
+, ['1133•French']:'M', 
+['15•Indonesian']:'N'
+, ['1133•Indonesian']:'M', 
+['16•Japanese']:'N'
+, ['182•Japanese']:'N'
 }
 WARNS = {
-    'ゴースト•Japanese' : ['*', 'WARNING: ゴースト could mean either Ghost (type) or Haunter (pokemon)! This code defaults to Ghost, please edit the output if you want Haunter','WARNING: ゴースト means both Ghost (type) and Haunter (pokemon) [Unknown Priority], so your output may be different than expected'], 
-    'VOL•French' : ['M', 'WARNING: Vol could mean either Flying (type) or Fly (move)! This code defaults to @Fly, please edit the output if you want @Flying','WARNING: Vol means both Flying (type) and Fly (move) [type taking priority], so your output may be different than expected'], 
-    'TERBANG•Indonesian' : ['M', 'WARNING: Terbang could mean either Flying (type) or Fly (move)! This code defaults to @Fly, please edit the output if you want @Flying','WARNING: Terbang means both Flying (type) and Fly (move) [type taking priority], so your output may be different than expected'], 
-    'PSYCHIC•English' : ['M', 'WARNING: Psychic could mean either Psychic (type) or Psychic (move)! This code translates the type properly but always considers @Psychic as (Move), please edit the output if you want @Psychic (type)','WARNING: Psychic means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected'], 
-    'साइकक•Hindi' : ['M', 'WARNING: साइकक could mean either Psychic (type) or Psychic (move)! This code translates the type properly but always considers @Psychic as (Move), please edit the output if you want @Psychic (type)','WARNING: साइकक means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected'], 
-    'PSÍQUICO•Brazilian Portuguese' : ['M', 'WARNING: Psíquico could mean either Psychic (type) or Psychic (move)! This code translates the type properly but always considers @Psychic as (Move), please edit the output if you want @Psychic (type)','WARNING: Psíquico means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected'], 
-    'PSÍQUICO•Spanish' : ['M', 'WARNING: Psíquico could mean either Psychic (type) or Psychic (move)! This code translates the type properly but always considers @Psychic as (Move), please edit the output if you want @Psychic (type)','WARNING: Psíquico means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected'], 
-    'PSIŞIK•Turkish' : ['M', 'WARNING: Psişik could mean either Psychic (type) or Psychic (move)! This code translates the type properly but always considers @Psychic as (Move), please edit the output if you want @Psychic (type)','WARNING: Psişik means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected']
+'ゴースト•Japanese' : ['*', 'WARNING: ゴースト could mean either Ghost (type) or Haunter (pokemon)! This page defaults to Ghost, please edit the output if you want Haunter','WARNING: ゴースト means both Ghost (type) and Haunter (pokemon) [Unknown Priority], so your output may be different than expected'], 
+'VOL•French' : ['M', 'WARNING: Vol could mean either Flying (type) or Fly (move)! This page defaults to @Fly, please edit the output if you want @Flying','WARNING: Vol means both Flying (type) and Fly (move) [type taking priority], so your output may be different than expected'], 
+'TERBANG•Indonesian' : ['M', 'WARNING: Terbang could mean either Flying (type) or Fly (move)! This page defaults to @Fly, please edit the output if you want @Flying','WARNING: Terbang means both Flying (type) and Fly (move) [type taking priority], so your output may be different than expected'], 
+'PSYCHIC•English' : ['M', 'WARNING: Psychic could mean either Psychic (type) or Psychic (move)! This page defaults to @Psychic (Move), please edit the output if you want @Psychic (type)','WARNING: Psychic means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected'], 
+'साइकक•Hindi' : ['M', 'WARNING: साइकक could mean either Psychic (type) or Psychic (move)! This page defaults to @Psychic (Move), please edit the output if you want @Psychic (type)','WARNING: साइकक means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected'], 
+'PSÍQUICO•Brazilian Portuguese' : ['M', 'WARNING: Psíquico could mean either Psychic (type) or Psychic (move)! This page defaults to @Psychic (Move), please edit the output if you want @Psychic (type)','WARNING: Psíquico means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected'], 
+'PSÍQUICO•Spanish' : ['M', 'WARNING: Psíquico could mean either Psychic (type) or Psychic (move)! This page defaults to @Psychic (Move), please edit the output if you want @Psychic (type)','WARNING: Psíquico means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected'], 
+'PSIŞIK•Turkish' : ['M', 'WARNING: Psişik could mean either Psychic (type) or Psychic (move)! This page defaults to @Psychic (Move), please edit the output if you want @Psychic (type)','WARNING: Psişik means both Psychic (type) and Psychic (move) [type taking priority], so your output may be different than expected']
 }
-
 LOCALES = {
-    'English':'en',
-    'Brazilian Portuguese':'pt-BR',
-    'Chinese Traditional':'zh',
-    'French':'fr',
-    'German':'de',
-    'Hindi':'hi',
-    'Indonesian':'id',
-    'Italian':'it',
-    'Japanese':'ja',
-    'Korean':'ko',
-    'Russian':'ru',
-    'Spanish':'es',
-    'Thai':'th',
-    'Turkish':'tr'
+'English':'en',
+'Brazilian Portuguese':'pt-BR',
+'Chinese Traditional':'zh',
+'French':'fr',
+'German':'de',
+'Hindi':'hi',
+'Indonesian':'id',
+'Italian':'it',
+'Japanese':'ja',
+'Korean':'ko',
+'Russian':'ru',
+'Spanish':'es',
+'Thai':'th',
+'Turkish':'tr'
 }
